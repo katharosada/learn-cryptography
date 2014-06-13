be44f9dbc2f89424bfb387604c76f20f1d9d4401
Katie Bell <katie@katharos.id.au>
2014-06-13 23:53:00 +0200

    Use Chart.js to construct frequency analysis graphs, and hook into a directive using chartjs-directive

--- learn-cryptography/client/js/app.js
+++ learn-cryptography/client/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('cryptoApp', [
   'ngRoute',
+  'chartjs-directive',
   'cryptoApp.filters',
   'cryptoApp.services',
   'cryptoApp.directives',
--- learn-cryptography/client/js/controllers.js
+++ learn-cryptography/client/js/controllers.js
@@ -2,6 +2,34 @@
 
 /* Controllers */
 
+var getFrequencies = function(text) {
+  text = text.toLowerCase();
+  var freqs = {};
+  var maxFreq = 0;
+  for ( var i = 0; i < text.length; i++ ) {
+      if ('a' <= text[i] && text[i] <= 'z') {
+          freqs[text[i]] = (freqs[text[i]] || 0) + 1;
+          if (freqs[text[i]] > maxFreq) {
+            maxFreq = freqs[text[i]];
+          }
+      }
+  }
+  console.log(freqs);
+  var freqList = [];
+  var aleph = "abcdefghijklmnopqrstuvwxyz".split("");
+  var scale = 13/maxFreq;
+  for (var i = 0; i < aleph.length; i++) {
+    freqList.push(scale * freqs[aleph[i]]);
+  }
+  return freqList;
+};
+
+var fillCharts = function($scope) {
+  $scope.textFreqChart.data.datasets[0].data = getFrequencies($scope.level.text.encrypted);
+};
+
+
+
 angular.module('cryptoApp.controllers', [])
   .controller('IndexController', ['$scope', function($scope) {
   }])
@@ -12,6 +40,7 @@ angular.module('cryptoApp.controllers', [])
         $scope.level = data;
         // New level, default to not-yet-complete.
         $scope.level.success = false;
+        fillCharts($scope);
       }
     );
 
@@ -29,7 +58,7 @@ angular.module('cryptoApp.controllers', [])
       return result;
     };
 
-    $scope.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    $scope.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
 
     $scope.rotate_decryptor = {"id":"rotate", "key": {"rotate": 1}};
     $scope.translate_decryptor = {"id":"translate", "key":{}}
@@ -56,4 +85,65 @@ angular.module('cryptoApp.controllers', [])
           });
     };
 
+    var englishFreqData = {
+      labels : $scope.alphabet,
+      datasets : [{
+        fillColor : "rgba(151,187,205,0.5)",
+        strokeColor : "rgba(151,187,205,1)",
+        data : [
+           8.167,
+           1.492,
+           2.782,
+           4.253,
+           12.70,
+           2.228,
+           2.015,
+           6.094,
+           6.966,
+           0.153,
+           0.772,
+           4.025,
+           2.406,
+           6.749,
+           7.507,
+           1.929,
+           0.095,
+           5.987,
+           6.327,
+           9.056,
+           2.758,
+           0.978,
+           2.360,
+           0.150,
+           1.974,
+           0.074
+        ]
+      }]
+    }
+    var options = {
+        scaleShowLabels : false,
+        animation: false,
+        scaleOverride: true,
+        scaleSteps: 13,
+        scaleStepWidth: 1,
+        scaleStartValue: 0,
+        barValueSpacing: 1
+    };
+
+    $scope.englishFreqChart = {
+      options: options,
+      data : englishFreqData
+    };
+
+    $scope.textFreqChart = {
+      options: options,
+      data : {
+        labels: $scope.alphabet,
+        datasets : [{
+          fillColor : "rgba(151,187,205,0.5)",
+          strokeColor : "rgba(151,187,205,1)",
+          data : getFrequencies("")
+        }]
+      }
+    };
   }]);
