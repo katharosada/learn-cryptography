5863c858022288f99edbf45d99803998ebfc7029
Katie Bell <katie@katharos.id.au>
2014-06-14 16:38:04 +0200

    Migrate the other uses of tabs to ui.bootstrap tabset directive and remove old jquery-based tab directive.

--- learn-cryptography/client/js/directives.js
+++ learn-cryptography/client/js/directives.js
@@ -10,14 +10,3 @@ directives.directive('appVersion', ['version', function(version) {
       elm.text(version);
     };
   }]);
-
-directives.directive('showtab', function () {
-        return {
-            link: function (scope, element, attrs) {
-                element.click(function(e) {
-                    e.preventDefault();
-                    $(element).tab('show');
-                });
-            }
-        };
-    });
