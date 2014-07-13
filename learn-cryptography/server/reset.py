00b242a1d6bb6e1c9088b87efb3d97a5895c4fdd
Katie Bell <katie@katharos.id.au>
2014-07-13 15:05:05 +0200

    Separate the Caesar ciphers and substitution ciphers into two separate level sequences.

--- learn-cryptography/server/reset.py
+++ learn-cryptography/server/reset.py
@@ -49,62 +49,69 @@ def appendToLevelSequence(level_sequence, level_id_list):
           level_sequence.levels.append(level_key)
           level_sequence.put()
 
-def resetAllTheThings():
-    level_sequence_key = ndb.Key(model.LevelSequence, model.LEVEL_LIST)
+def resetLevelSequence(level_sequence_id, name):
+    level_sequence_key = ndb.Key(model.LevelSequence, level_sequence_id)
     level_sequence = level_sequence_key.get()
     if not level_sequence:
         # Create level list with level 1
         level_sequence = model.LevelSequence(key=level_sequence_key)
-        level_sequence.name = 'default'
-        level_sequence.put()
+    level_sequence.name = name
+    level_sequence.put()
+    return level_sequence
+
+def resetAllTheThings():
+    caesar_sequence = resetLevelSequence(model.LEVEL_LIST, 'Caesar Ciphers')
 
     # Level 1 - Caesar Cipher
     resetText('caesar1', 'rotate', '{"rotate": "3"}')
-    resetStandardDecryptLevel(level_sequence_key, 'caesar1', 'caesar1', ['caesar2'])
+    resetStandardDecryptLevel(caesar_sequence.key, 'caesar1', 'caesar1', ['caesar2'])
 
     # Caesar cipher 2 (ROT 13)
     resetText('caesar2', 'rotate', '{"rotate": "13"}')
-    resetStandardDecryptLevel(level_sequence_key, 'caesar2', 'caesar2', ['caesar3'])
+    resetStandardDecryptLevel(caesar_sequence.key, 'caesar2', 'caesar2', ['caesar3'])
 
     # Caesar cipher 3 (rotate -5)
     resetText('caesar3', 'rotate', '{"rotate": "21"}')
-    resetStandardDecryptLevel(level_sequence_key, 'caesar3', 'caesar3', ['translation1'])
+    resetStandardDecryptLevel(caesar_sequence.key, 'caesar3', 'caesar3', ['translation1'])
 
-    appendToLevelSequence(level_sequence, ['caesar1', 'caesar2', 'caesar3'])
+    appendToLevelSequence(caesar_sequence, ['caesar1', 'caesar2', 'caesar3'])
 
-    # Translation ciphers sequence
+    # Substitution/Translation ciphers sequence
+    translation_sequence = resetLevelSequence('substitution', "Substitution Ciphers")
+
     def alephmap(newaleph):
       result = {}
       for a, b in zip(string.lowercase, newaleph):
         result[a] = b
       return json.dumps(result)
 
-    # Backwards alphabet translation
+    # Backwards alphabet substitution
     resetText('translation1', 'translate', alephmap('zyxwvutsrqponmlkjihgfedcba'))
-    resetStandardDecryptLevel(level_sequence_key, 'translation1', 'translation1', ['translation2'])
+    resetStandardDecryptLevel(translation_sequence.key, 'translation1', 'translation1', ['translation2'])
 
-    # Translation Cipher 2: Vowels and constonants shifted separately
+    # Substitution Cipher 2: Vowels and constonants shifted separately
     resetText('translation2', 'translate', alephmap('ecdfighjoklmnpuqrstvawxyzb'))
-    resetStandardDecryptLevel(level_sequence_key, 'translation2', 'translation2', ['translation3'])
+    resetStandardDecryptLevel(translation_sequence.key, 'translation2', 'translation2', ['translation3'])
 
-    # Translation Cipher 2: Scrambled alphabet
+    # Substitution Cipher 2: Scrambled alphabet
     resetText('translation3', 'translate', alephmap('kgxmqblwrjnydshzatoeuifpvc'))
-    resetStandardDecryptLevel(level_sequence_key, 'translation3', 'translation3', [])
+    resetStandardDecryptLevel(translation_sequence.key, 'translation3', 'translation3', [])
 
-    appendToLevelSequence(level_sequence, ['translation1', 'translation2', 'translation3'])
+    appendToLevelSequence(translation_sequence, ['translation1', 'translation2', 'translation3'])
 
-    return level_sequence
+    return [caesar_sequence, translation_sequence]
 
 
 class ResetHandler(webapp2.RequestHandler):
     def get(self):
-        level_sequence = resetAllTheThings()
+        level_sequences = resetAllTheThings()
 
         level_data = []
-        for level_key in level_sequence.levels:
-            level_data.append(level_key.get())
+        for level_sequence in level_sequences:
+          for level_key in level_sequence.levels:
+              level_data.append(level_key.get())
 
-        values = {'level_list':level_sequence, 'level_data':level_data}
+        values = {'level_list':level_sequences, 'level_data':level_data}
 
         template = jinja_environment.get_template('reset.html')
         self.response.out.write(template.render(values))
